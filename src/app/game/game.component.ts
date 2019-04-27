4955960db53a1fe016595a8a1844d3dafc959a65
Gabriel Toschi <gabriel.toschi@hotmail.com>
2019-04-27 15:33:18 -0300

    1HGJ version

---
+++ src/app/game/game.component.ts
@@ -0,0 +1,79 @@
+import { Component, OnInit } from '@angular/core';
+import { timeCards, Trivia } from '../data/timecards';
+
+@Component({
+  selector: 'app-game',
+  templateUrl: './game.component.html',
+  styleUrls: ['./game.component.sass']
+})
+export class GameComponent implements OnInit {
+
+  cards: Trivia[];
+  points: number = 0;
+  endGame = false;
+
+  leftCard: Trivia;
+  rightCard: Trivia;
+
+  constructor(
+  ) { }
+
+  ngOnInit() {
+    this.cards = [
+      {
+        name: 'Teste',
+        imageUrl: 'aaa',
+        day: 1,
+        month: 1,
+        year: 2000,
+      },
+      {
+        name: 'Teste2',
+        imageUrl: 'aaa',
+        day: 1,
+        month: 1,
+        year: 2010,
+      },
+    ];
+
+    this.cards = timeCards;
+    this.getCards();
+  }
+
+  getCards() {
+    this.leftCard = this.cards[Math.floor(Math.random() * this.cards.length)];
+
+    do {
+      this.rightCard = this.cards[Math.floor(Math.random() * this.cards.length)];
+    } while (this.leftCard == this.rightCard);
+  }
+
+  chooseCard(chosen: 'left' | 'right') {
+    if (chosen === this.compareCards(this.leftCard, this.rightCard)) {
+      this.points++;
+      this.getCards();
+    } else {
+      this.endGame = true;
+    }
+  }
+
+  compareCards(a: Trivia, b: Trivia) {
+    const aDate = new Date(a.year, a.month, a.day);
+    const bDate = new Date(b.year, b.month, b.day);
+
+    return aDate < bDate ? 'left' : 'right';
+  }
+
+  reset() {
+    this.endGame = false;
+    this.points = 0;
+    this.getCards();
+  }
+
+  shareTwitter() {
+    const url = `http://twitter.com/share?text=I managed to put ${this.points} Time's in the right time order. Can you get more than me?&url=http://aaaaa&hashtags=timingtime, 1hgj`;
+
+    window.open(url, '_blank');
+  }
+
+}
